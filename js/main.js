de84a77a8188f6f1f0f29b32c1532e65a301b85d
Alfred Asante Apenteng <alfredapenteng6996@gmail.com>
2024-11-14 14:18:28 +0000

    feat: countdown

--- js/main.js
+++ js/main.js
@@ -54,3 +54,36 @@
     return false;
   });
 })(jQuery);
+
+// Set the date we're counting down to November 23rd, 2024
+const countDownDate = new Date("November 23, 2024 08:00:00").getTime();
+
+// Update the countdown every second
+const x = setInterval(function () {
+  const now = new Date().getTime();
+  const distance = countDownDate - now;
+
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("days").innerHTML = days.toString().padStart(2, "0");
+  document.getElementById("hours").innerHTML = hours
+    .toString()
+    .padStart(2, "0");
+  document.getElementById("minutes").innerHTML = minutes
+    .toString()
+    .padStart(2, "0");
+  document.getElementById("seconds").innerHTML = seconds
+    .toString()
+    .padStart(2, "0");
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML =
+      "<h1>EVENT HAS STARTED</h1>";
+  }
+}, 1000);
